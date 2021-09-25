2c97e5552acc4b5536dbc1fd209450f2f5db42f4
reikotavares <anatvpoli@gmail.com>
2021-09-25 17:04:05 -0300

    alteração sqlite > mysql

--- project/settings.py
+++ project/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sistemahp',
+        'PORT': '3306',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+
     }
 }
 
